44f2eaaa32100336ac7ca88e64bfea78dfd73bb2
Taylor <tw10262@gmail.com>
2021-03-15 21:10:00 -0700

    inital commit

---
+++ Day2/hwNotes.js
@@ -0,0 +1,14 @@
+
+
+                // Git Vocab
+
+// Add - Include a file in git which wasn't there before
+// Clone - Create a local copy of a git repository which is hosted in an online service (e.g. GitHub)
+// Commit (verb) - To lock in a set of changes
+// Commit (noun) - A set of changes that has been locked in
+// Modified - Status of a file that has changes in it
+// Origin - Default name for the remote server
+// Pull - Retrieve the latest files from online service (a.k.a. "pull from origin")
+// Push - Send the latest files to the online service (a.k.a. "push to origin")
+// Remove - Take a file out of git which was previously added
+// Repository - Collection of files in git that belong together - called "repo", for short; for example,
